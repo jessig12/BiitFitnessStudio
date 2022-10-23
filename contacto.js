309be05bf90930f1cfdaae62eb7839d6afcbe63a
kevinherreroslechuga <113234810+kevinherreroslechuga@users.noreply.github.com>
2022-10-23 14:04:14 -0500

    Actualizacion

--- contacto.js
+++ contacto.js
@@ -1,24 +1,26 @@
-const form = document.querySelector(".registro");
-form.addEventListener("submit", e => { 
-    e.preventDefault();
-    let name = document.querySelector(".name").value;
-    let email = document.querySelector(".email").value;
+const btnverificar = document.getElementById('boton');
 
-    document.querySelector(".registro").reset();
-    sendEmail(name, email);
-})
-function sendEmail(name, email){
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "kev.herreros@gmail.com",
-    Password:"98F827AFCA279DAAEEE1490452EF1CBB522F",
-    To: 'kev.herreros@gmail.com',
-    From:document.getElementById("email").value,
-    Subject:"New Contact Form",
-    Body: document.getElementById("message").value,
-}).then((success) => {
-     alert("Mensaje enviado");
-}).catch((error)=> {
-    alert("mensaje no enviado ");
-})
-}
+
+
+
+
+
+
+const btn = document.getElementById('boton');
+
+document.getElementById('biit')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   const serviceID = 'default_service';
+   const templateID = 'template_t5fx8vs';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Agregar';
+      alert('Mensaje enviado');
+    }, (err) => {
+      btn.value = 'Agregar';
+      alert(JSON.stringify(err));
+    });
+});
