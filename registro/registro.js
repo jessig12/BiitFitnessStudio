c151d0c0f27c7f928c5b06183279191ca3c52e17
Elizabeth <elizabethcrisostomo09@hotmail.com>
2022-10-25 23:35:41 -0500

    actualizaciones

--- registro/registro.js
+++ registro/registro.js
@@ -3,5 +3,39 @@ const lastName = document.getElementById("inputLastName");
 const email = document.getElementById("inputEmail4");
 const password = document.getElementById("inputPassword4");
 const movil = document.getElementById("inputMovil");
-const city = document.getElementById("inputCity");
-const state = document.getElementById("inputAddress");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+
+
+form.addEventListener("submit", e=>{
+  e.preventDefault()
+  let warnings = ""
+  let entrar = false
+  let regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/
+  let regex = /[^+\d]/
+  parrafo.innerHTML = ""
+
+  if(firtsName.value.length < 6){
+     warnings += `El nombre no es valido <br>`
+  }
+  
+  if (regexEmail.test(email.value)){
+    warnings += `El email no es valido <br>`
+    entrar = true
+  }
+  if (regex.test(movil.value)){
+    warnings += `El telefono no es valido <br>`
+    entrar = true
+  }
+
+  if (password.value.length < 5){
+    warnings += `La contraseña no es valida <br>`
+    entrar = true
+  }
+  if(entrar){
+    parrafo.innerHTML = warnings
+ }
+ else{
+  parrafo.innerHTML = "Registro exitosamente"
+ }
+})
