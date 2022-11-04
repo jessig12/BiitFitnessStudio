357b49e8f24552284c39cb4016feb954086e15f5
Elizabeth <elizabethcrisostomo09@hotmail.com>
2022-11-03 23:47:19 -0600

    actualizaciones

--- public/login.js
+++ public/login.js
@@ -1,27 +1,26 @@
 
 
-function entrar(){
+function entrar() {
   const email = document.getElementById('inputEmail4').value;
   const password = document.getElementById('inputPassword').value;
 
- if (email == "elizabeth@gmail.com" && password == '12345678'){
-window.location.href = '/';
-localStorage.usuario = email
- }
- else if (email == "jessi@gmail.com" && password == '1234567'){
-  window.location.href = '/';
-  localStorage.usuario = email
-   }
-   else if (email == "kevin@gmail.com" && password == '123456'){
+  if (email == "elizabeth@gmail.com" && password == '12345678') {
     window.location.href = '/';
     localStorage.usuario = email
-     }
-     else if (email == "rodrigo@gmail.com" && password == '12345678'){
-      window.location.href = '/';
-      localStorage.usuario = email
-       }else{
-  alert('Error contraseña o correo incorrecto')
- }
+  }
+  else if (email == "jessi@gmail.com" && password == '1234567') {
+    window.location.href = '/';
+    localStorage.usuario = email
+  }
+  else if (email == "kevin@gmail.com" && password == '123456') {
+    window.location.href = '/';
+    localStorage.usuario = email
+  }
+  else if (email == "rodrigo@gmail.com" && password == '12345678') {
+    window.location.href = '/';
+    localStorage.usuario = email
+  } else {
+    alert('Error contraseña o correo incorrecto')
+  }
 
 }
-
