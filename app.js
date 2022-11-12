870c404efe1e5fb626edd4949bcb2320bc24f52f
Elizabeth <elizabethcrisostomo09@hotmail.com>
2022-11-11 21:34:20 -0600

    actualizaciones

--- app.js
+++ app.js
@@ -59,6 +59,85 @@ app.get('/bienvenido', (req, res) => {
 
 
 
+/////////////////Codigo para usar en posman o insomnia////////////////////////////////
+
+app.post('/api/registo/guardar', (req, res) => {
+
+  let rawdata = fs.readFileSync('./registro.json'); // json file is db in memory
+  let registros = JSON.parse(rawdata);
+
+  if (!registros.data.find((usuario) => usuario.email === req.body.email)) {
+    registros.data.push(req.body);
+
+    fs.writeFileSync('registro.json', JSON.stringify(registros, null, 2));
+    console.log("registro guardado")
+    res.send({ status: "Usuario guardado" })
+  } else {
+    console.log("ya existe un usuario con este correo")
+    res.send({ status: "error", message: "email ya registrado" })
+  }
+});
+
+app.post('/api/validar-login', (req, res) => {
+
+  let rawdata = fs.readFileSync('./registro.json'); // json file is db in memory
+  let registros = JSON.parse(rawdata);
+
+  if (registros.data.find((usuario) => usuario.email === req.body.email && usuario.password === req.body.password)) {
+    console.log("usuario y contrasena validos")
+    res.send({ status: "ok" })
+  } else {
+    console.log("usuario y contrasena no validos")
+    res.send({ status: "error", message: "usuario y contrasena no validos" })
+  }
+});
+
+//////////////////////LOGIN/////////////////////////////////////
+app.get('/api/registro/:abc', (req, res) => {
+
+  let rawdata = fs.readFileSync('./registro.json'); // json file is db in memory
+  let registros = JSON.parse(rawdata);
+  const usuario = registros.data.find((usuario) => usuario.id + "" === req.params.abc);
+
+  if (usuario) {
+    res.send({ status: "ok", usuario })
+  } else {
+    res.send({ status: "error", message: "no existe" })
+  }
+
+});
+
+//////////// Productos ////////////////////////////
+
+app.get('/api/productos', (req, res) => {
+  let rawdata = fs.readFileSync('./productos.json');
+  let productos = JSON.parse(rawdata);
+  res.send({ status: "ok", productos })
+});
+
+
+app.post('/api/compras/nueva', (req, res) => {
+  let rawdata = fs.readFileSync('./compras.json');
+  let compras = JSON.parse(rawdata);
+
+  if (true) { // validaciones: tdc, stock, etc
+    compras.push(req.body)
+    fs.writeFileSync('compras.json', JSON.stringify(compras, null, 2));
+    res.send({ status: "ok", "msg": "compra realizada" })
+
+  } else {
+
+    res.send({ status: "error", "msg": "fondos insuficientes" })
+  }
+});
+
+
+app.get('/api/compras/:userId', (req, res) => {
+  let rawdata = fs.readFileSync('./compras.json');
+  let compras = JSON.parse(rawdata);
+  res.send({ status: "ok", compras: compras.filter((compra) => compra.userId + "" === req.params.userId) })
+});
+
 
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${process.env.PORT || 3000}!`));
